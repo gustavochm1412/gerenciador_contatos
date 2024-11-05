6d65f763c8d36d3624b24967b45ead963a129a36
Gustavo Henrique Cordeiro <90660233+gustavochm1412@users.noreply.github.com>
2024-11-04 21:34:20 -0300

    Update lista_contatos_widget_test.dart

--- test/screens/lista_contatos_widget_test.dart
+++ test/screens/lista_contatos_widget_test.dart
@@ -6,13 +6,13 @@ import 'package:gerenciador_contatos/components/contato_card.dart';
 
 void main() {
   testWidgets('Exibir lista de contatos', (WidgetTester tester) async {
-    // Mock de contatos para o teste
+    
     final contatosMock = [
       Contato(id: 1, nome: 'João', telefone: '123456789', grupoId: 1),
       Contato(id: 2, nome: 'Maria', telefone: '987654321', grupoId: 2),
     ];
 
-    // Constrói o widget com a lista de contatos
+    
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
         body: ListView(
@@ -21,7 +21,7 @@ void main() {
       ),
     ));
 
-    // Verifica se os widgets ContatoCard aparecem corretamente
+    
     expect(find.byType(ContatoCard), findsNWidgets(2));
     expect(find.text('João'), findsOneWidget);
     expect(find.text('Maria'), findsOneWidget);
